cf832316f6a2645b811f651d8492e4a47c83c9d3
programmingArrow <nishant.kashyap2104@gmail.com>
2020-02-19 23:51:27 +0530

    first class

--- 1/NewClass.php
+++ 1/NewClass.php
@@ -1,5 +1,25 @@
 <?php
 
 class NewClass {
-    
+    private $firstName = 'John';
+    private $lastName = 'Wick';
+
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    public function getFullName()
+    {
+        return $this->firstName . ' ' . $this->lastName;
+    }
 }
+
+$object = new NewClass;
+
+var_dump($object);
